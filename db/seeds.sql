2d93436a42b62d400ef4e4623ba71bff520cd006
Spencer Fife <scottyfife17@gmail.com>
2020-02-22 10:04:04 -0800

    adding to schemas, git init, server js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Bacon Deluxe', true);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Veggie Burger', false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Mushroom Swiss', true);
--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const express = require("express");
+const expressHandlbars = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burger_db"
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting", err.stack);
+    return;
+  }
+
+  console.log(`connected with id ${connection.threadId}`);
+});
